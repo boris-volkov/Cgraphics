1321c296b63120295634e426f9e202c55cf51710
boris-volkov <borislink@protonmail.com>
2024-09-28 19:30:50 -0700

    vsync collisions

--- collisions/collision.c
+++ collisions/collision.c
@@ -12,10 +12,11 @@ typedef struct {
 	int radius;
 	vector position;
 	vector velocity;
+	uint32_t color;
 } ball;
 
 
-void wall_collision(ball *b){
+void wall_collision(ball *b){ // handles the collision and moves the guy back if needed
 	if ((b->position.x - b->radius) < 0    ) {b->velocity.x *= -1; b->position.x = 0 + b->radius;}
 	if ((b->position.x + b->radius) > 1920 ) {b->velocity.x *= -1; b->position.x = 1920-b->radius;}
 	if ((b->position.y - b->radius) < 0    ) {b->velocity.y *= -1; b->position.y = 0 + b->radius;}
@@ -26,7 +27,7 @@ double dot (vector v1, vector v2){// specifically used for velocities
 	return v1.x * v2.x + v1.y * v2.y;
 }
 
-double mag (vector v){
+double mag (vector v){ // magnitude of a vector
 	return sqrt(pow(v.x, 2) + pow(v.y, 2));	
 }
 
@@ -45,90 +46,118 @@ vector projection(vector a, vector b){ // projection of a onto b
 	return proj;
 }
 
-int touching (ball b1, ball b2){
+int touching (ball b1, ball b2){ 
 	return ( (b1.radius + b2.radius) > mag(difference(b1.position, b2.position)));
-}
-
-void collision(ball *b1, ball *b2){
-	vector normal = difference(b1->position, b2->position);
-	
-	vector tangent;
-	tangent.x = -normal.y;
-	tangent.y = normal.x;
-
-	vector b1_normal = projection(b1->velocity, normal);
-	vector b1_tangent = projection(b1->velocity, tangent);
-	b1_normal.x *= -1;
-	b1_normal.y *= -1;
-	b1->velocity.x = b1_normal.x + b1_tangent.x;
-	b1->velocity.y = b1_normal.y + b1_tangent.y;
-	
-	vector b2_normal = projection(b2->velocity, normal);
-	vector b2_tangent = projection(b2->velocity, tangent);
-	b2_normal.x *= -1;
-	b2_normal.y *= -1;
-	b2->velocity.x = b2_normal.x + b2_tangent.x;
-	
-	return;
 }
 
 void clear_ball(ball *b){
 		fill_circle(b->position.x, b->position.y, b->radius, BLACK);
 }
 
-void update_position(ball *b, uint32_t color){
+void draw_ball(ball *b){
+		fill_circle(b->position.x, b->position.y, b->radius, b->color);
+}
+
+void rewind_overlap(ball *b1, ball *b2){
+	while(touching(*b1, *b2)){
+		b1->position.x -= 0.1*b1->velocity.x;
+		b1->position.y -= 0.1*b1->velocity.y;
+		b2->position.x -= 0.1*b2->velocity.x;
+		b2->position.y -= 0.1*b2->velocity.y;
+	}
+}
+
+void collision(ball *b1, ball *b2){ // sends balls back after a collision
+	
+	vector normal = difference(b1->position, b2->position);
+	
+	vector tangent;
+	tangent.x = -normal.y;
+	tangent.y = normal.x;
+	
+	// should only turn them around if they are going in opposite directions
+	// what about it just sums their normal components?
+
+	vector b1_normal = projection(b1->velocity, normal);
+	vector b1_tangent = projection(b1->velocity, tangent);
+	vector b2_normal = projection(b2->velocity, normal);
+	vector b2_tangent = projection(b2->velocity, tangent);
+	
+	vector b1_temp;
+	b1_temp.x = b1_normal.x;
+	b1_temp.y = b1_normal.y;
+
+	b1_normal.x = b2_normal.x;
+	b1_normal.y = b2_normal.y;
+	b1->velocity.x = b1_normal.x + b1_tangent.x;
+	b1->velocity.y = b1_normal.y + b1_tangent.y;
+	
+	b2_normal.x = b1_temp.x;
+	b2_normal.y = b1_temp.y;
+	b2->velocity.x = b2_normal.x + b2_tangent.x;
+	b2->velocity.y = b2_normal.y + b2_tangent.y;
+}
+
+void update_position(ball *b){
 		b->position.x += b->velocity.x;
 		b->position.y += b->velocity.y;
-		fill_circle(b->position.x, b->position.y, b->radius, color);
 }
 
 int main(){
 	buffer_init();
-	clear_buffer();
+	//clear_buffer();
 
 	ball b;
 	b.radius = 30;
 	b.position.x = 1000;
 	b.position.y = 1000;
-	b.velocity.x = 3;
-	b.velocity.y = 4;
+	b.velocity.x = 0.3;
+	b.velocity.y = 0.4;
+	b.color = 0xffaa33;
 
 	ball c;
 	c.radius = 30;
 	c.position.x = 1040;
 	c.position.y = 300;
-	c.velocity.x = 3;
-	c.velocity.y = -2;
+	c.velocity.x = 0.3;
+	c.velocity.y = -0.2;
+	c.color = 0xff2233;
 
 	ball d;
 	d.radius = 30;
 	d.position.x = 340;
 	d.position.y = 600;
-	d.velocity.x = 5;
-	d.velocity.y = 2;
+	d.velocity.x = 0.5;
+	d.velocity.y = 0.2;
+	d.color = 0x3366ff;
 
-	int i = 100000;
+	ball e;
+	e.radius = 30;
+	e.position.x = 340;
+	e.position.y = 1000;
+	e.velocity.x = 0.2;
+	e.velocity.y = -0.3;
+	e.color = 0x33ff77;
+
+	ball balls[] = {b, c, d, e};
+	int ball_count = 4;
+
+	int i = 1000000;
 	while (i--){
-		clear_ball(&b);
-		clear_ball(&c);
-		clear_ball(&d);
-		wall_collision(&b);
-		wall_collision(&c);
-		wall_collision(&d);
-		update_position(&b, 0xffaa33);
-		update_position(&c, 0x3366ff);
-		update_position(&d, 0xff2233);
-		if (touching(b, c)){
-			collision(&b, &c);
+		for (int j = 0; j < ball_count; j++){
+			clear_ball(&balls[j]);
+			for (int other = j+1; other < ball_count ; other++){
+				if (touching(balls[j], balls[other])){
+					clear_ball(balls + other);
+					rewind_overlap(&balls[j], &balls[other]);
+					collision(&balls[j], &balls[other]);
+				}
+			}
+			wall_collision(&balls[j]);
+			update_position(&balls[j]);
+			draw_ball(balls + j);
 		}
-		if (touching(b, d)){
-			collision(&b, &d);
-		}
-		if (touching(d, c)){
-			collision(&d, &c);
-		}
-
-		usleep(10000);
+		usleep(1000);
 	}
 
 	buffer_reset();
